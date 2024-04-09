eb7f9642328b6a52496245a9b9c73ed57487b59c
jrichardsen <jonas.richardsen@gmail.com>
2024-04-09 08:45:16 +0200

    Change statusline

--- config/ui/statusline.nix
+++ config/ui/statusline.nix
@@ -1,12 +1,48 @@
 {
   config = {
-    plugins.mini = {
+    plugins.lualine = {
       enable = true;
-      modules.statusline = { };
+      sections = {
+        lualine_a = [ "mode" ];
+        lualine_b = [ 
+          "branch"  
+          {
+            name = "diff";
+            extraConfig.symbols = {
+              # TODO: add symbols
+            };
+          }
+        ];
+        lualine_c = [ 
+          {
+            name = "filetype"; 
+            extraConfig = {
+              icon_only = true;
+              separator = "";
+              padding = { left = 1; right = 0; };
+            };
+          }
+          {
+            name = "filename";
+            extraConfig = {
+              path = 1;
+              symbols = {
+                # TODO: add symbols
+              };
+            };
+          }
+        ];
+        lualine_x = [
+          {
+            name = "diagnostics";
+            extraConfig.symbols = { 
+              # TODO: add symbols
+            };
+          }
+        ];
+        lualine_y = [ "progress" ];
+        lualine_z = [ "location" ];
+      };
     };
-
-    extraConfigLua = ''
-      require('mini.statusline').section_location = function() return ''' end;
-    '';
   };
 }
